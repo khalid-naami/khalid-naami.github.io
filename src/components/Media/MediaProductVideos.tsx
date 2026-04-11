92b24185427dffae017b000cf150ae8fcbfb7dd4
khalidnaami7 <khalidnaami3@gmail.com>
2026-04-12 00:38:52 +0100

    Create MediaProductVideos.tsx

---
+++ src/components/Media/MediaProductVideos.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import VideosList from '../General/VideosList';
+
+const data: any[] = [];
+
+export default function MediaProductVideos() {
+  return <VideosList videos={data} />;
+}
