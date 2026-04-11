509fc34c1cf0667225f737edc3fafa3e12d7bc45
khalidnaami7 <khalidnaami3@gmail.com>
2026-04-12 00:38:50 +0100

    Create MediaPodcasts.tsx

---
+++ src/components/Media/MediaPodcasts.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PodcastsList from "../General/PodcastsList";
+
+const data: any[] = [];
+
+export default function MediaPodcasts() {
+	return <PodcastsList podcasts={data} />;
+}
