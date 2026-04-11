51c4dc09309ba6fcf8045af30a1ef5ffdc541867
khalidnaami7 <khalidnaami3@gmail.com>
2026-04-12 00:38:57 +0100

    Create MediaWebinars.tsx

---
+++ src/components/Media/MediaWebinars.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import VideosList from "../General/VideosList";
+
+const data: any[] = [];
+
+export default function MediaWebinars() {
+	return <VideosList videos={data} />;
+}
