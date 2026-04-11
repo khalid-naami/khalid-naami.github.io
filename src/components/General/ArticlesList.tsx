bcb20e351ec1814198ee5dff01ab25b0dabc0bea
khalidnaami7 <khalidnaami3@gmail.com>
2026-04-12 00:37:51 +0100

    Create ArticlesList.tsx

---
+++ src/components/General/ArticlesList.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+interface Article {
+  title: string;
+  conference: string;
+  link?: string;
+}
+
+interface ArticleListProps {
+  articles: Article[];
+}
+
+export default function ArticlesList({ articles }: ArticleListProps) {
+  return (
+    <div className="mx-auto mt-8">
+      {articles.map((article, index) => (
+        <div key={index} className="mission-container">
+          <div className="mission-description">
+            <div className='text-xs mb-2'>
+              {article.conference}
+            </div>
+            <div className="font-semibold text-base mb-4">
+              {article.title}
+            </div>
+          </div>
+          {article.link && (
+            <div className="mission-buttons">
+              <a
+                href={article.link}
+                rel="noopener noreferrer"
+                target="_blank"
+                className="mission-button mission-button--join"
+              >
+                Learn more
+              </a>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
