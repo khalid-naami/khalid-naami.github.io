aee7a51052e43732f7cc7351b6cd61ce0a655104
khalidnaami7 <khalidnaami3@gmail.com>
2026-04-25 23:02:30 +0100

    Update newsletter.js

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,10 +1,26 @@
-import { useEffect } from 'react';
-import { useHistory } from '@docusaurus/router';
+import React, { useEffect } from 'react';
+import Layout from '@theme/Layout';
 
 export default function Newsletter() {
   useEffect(() => {
     window.location.href = 'https://substack.com/@khnaami';
   }, []);
 
-  return null;
+  return (
+    <Layout title="Newsletter Redirect">
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50vh',
+        fontSize: '1.5rem',
+        flexDirection: 'column',
+        textAlign: 'center',
+        padding: '2rem'
+      }}>
+        <h1 style={{ marginBottom: '1rem' }}>Redirecting to Newsletter...</h1>
+        <p>If you are not redirected automatically, <a href="https://substack.com/@khnaami">click here</a>.</p>
+      </div>
+    </Layout>
+  );
 }
