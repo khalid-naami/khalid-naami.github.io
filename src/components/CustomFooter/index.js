3c4ab688cfdc996ddfb3018bc728517bae1cad88
khalidnaami7 <khalidnaami3@gmail.com>
2026-04-25 13:02:54 +0100

    Update index.js

--- src/components/CustomFooter/index.js
+++ src/components/CustomFooter/index.js
@@ -46,17 +46,34 @@ export default function CustomFooter() {
             <div className="col text-center">
               <h4 className="mb-4">Let's stay in touch.</h4>
               <div className="flex justify-center space-x-4">
-                <a href="https://x.com/GamesOfOptions" target="_blank" rel="noreferrer" className="text-black dark:text-white hover:text-ds-blue-primary dark:hover:text-ds-blue-accent">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-label="Twitter">
-                    <title>Twitter</title>
+                {/* Personal Socials */}
+                <a href="https://x.com/khalidnaami0" target="_blank" rel="noreferrer" className="text-black dark:text-white hover:text-ds-blue-primary dark:hover:text-ds-blue-accent" title="Personal X">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
                   </svg>
                 </a>
-                <a href="https://www.linkedin.com/in/khalid-naami-a953263bb/" target="_blank" rel="noreferrer" className="text-black dark:text-white hover:text-ds-blue-primary dark:hover:text-ds-blue-accent">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-label="LinkedIn">
-                    <title>LinkedIn</title>
+                <a href="https://www.linkedin.com/in/khalid-naami-a953263bb/" target="_blank" rel="noreferrer" className="text-black dark:text-white hover:text-ds-blue-primary dark:hover:text-ds-blue-accent" title="Personal LinkedIn">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                   </svg>
+                </a>
+
+                {/* Dashboard Options Socials */}
+                <a href="https://x.com/DashboardOption" target="_blank" rel="noreferrer" className="text-black dark:text-white hover:text-ds-blue-primary dark:hover:text-ds-blue-accent" title="Dashboard Options X">
+                  <div className="flex flex-col items-center leading-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+                      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+                    </svg>
+                    <span className="text-[10px] font-bold mt-0.5">DO</span>
+                  </div>
+                </a>
+                <a href="https://www.linkedin.com/company/dashboard-options/" target="_blank" rel="noreferrer" className="text-black dark:text-white hover:text-ds-blue-primary dark:hover:text-ds-blue-accent" title="Dashboard Options LinkedIn">
+                  <div className="flex flex-col items-center leading-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+                      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                    </svg>
+                    <span className="text-[10px] font-bold mt-0.5">DO</span>
+                  </div>
                 </a>
                 <a href="https://github.com/khalid-naami" target="_blank" rel="noreferrer" className="text-[#333] dark:text-[#f5f5f5] hover:text-ds-blue-primary dark:hover:text-ds-blue-accent">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-label="GitHub">
