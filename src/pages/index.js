4f4eacbd9aed67c42d943f14cb15720110963645
khalidnaami7 <khalidnaami3@gmail.com>
2026-04-30 12:01:17 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -205,6 +205,8 @@ export default function Home() {
         <meta name="apple-mobile-web-app-title" content="Khalid Naami" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500;600;700&display=swap" rel="stylesheet" />
         
         {/* Resource Hints for Performance */}
         <link rel="preconnect" href="https://www.googletagmanager.com" />
