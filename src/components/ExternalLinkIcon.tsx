d04fa1554deb656320fa0d6c59f473e799598717
khalidnaami7 <khalidnaami3@gmail.com>
2026-05-05 20:03:19 +0100

    Update ExternalLinkIcon.tsx

--- src/components/ExternalLinkIcon.tsx
+++ src/components/ExternalLinkIcon.tsx
@@ -12,8 +12,8 @@ const ExternalLinkIcon = ({ size = 13, color = 'currentColor' }) => (
         strokeLinecap="round"
         strokeLinejoin="round"
         style={{ display: 'inline-block' }}
+        aria-hidden="true"
     >
-        <title>External link</title>
         <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
         <polyline points="15 3 21 3 21 9" />
         <line x1="10" y1="14" x2="21" y2="3" />
