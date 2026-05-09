c64d040a8fbe636b87a15088878554d57efe004e
khalidnaami7 <khalidnaami3@gmail.com>
2026-05-09 01:08:51 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ module.exports = async function createConfig() {
   return {
     title: 'Khalid Naami',
     tagline: 'Founder & CEO @ Dashboard Options | Quantitative Trading, Financial AI & Data Science.',
-    favicon: 'img/zoro-final.png',
+    favicon: 'img/zoro-final.webp',
 
   url: 'https://khalidnaami.com',
   // Set the /<baseUrl>/ pathname under which your site is served
